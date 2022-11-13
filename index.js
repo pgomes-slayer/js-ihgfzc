c38509a399a5149ac1be6e451d0dc73b6b530138
pgomes-slayer <pgomes@activewhere.com>
2022-11-13 12:54:11 +0000

    2 files modified

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter 3</h1>`;
 
 console.log('Bom dia');
+
+const appDiv2 = document.getElementById('app2');
+appDiv2.innerHTML = '<h5>JS Starter 3</h5>';
